c71178a4622d2a02ddd37189050428ec46a72c89
Jcobbi4644k <judith_cobbinah@cable.comcast.com>
2021-07-21 02:56:04 -0400

    Assignment 3 Progress 2

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -5,8 +5,9 @@ import org.launchcode.techjobsmvc.models.Job;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.PostMapping;
+
 
 import java.util.ArrayList;
 
@@ -23,23 +24,14 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping(value = "/results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all")) {
-            if (searchTerm.isEmpty()) {
-                jobs = JobData.findAll();
-                model.addAttribute("title", "All Jobs");
-            } else {
-                jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-                model.addAttribute("title", "Jobs with " + columnChoices(searchType) + ": " + searchTerm);
-            }
 
-        } else {
-            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
-            }
+    @RequestMapping(value = "/results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<Job> jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+
         model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
 
         return "list-jobs";
 
